400476b263d04d2cf72c2e7d32279a5d25365657
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-08-15 10:44:33 +0100

    fix epiparameter-cfr integration test using onset-to-death delay

--- tests/testthat/test-epiparameter-cfr.R
+++ tests/testthat/test-epiparameter-cfr.R
@@ -1,23 +1,24 @@
-# load a single Ebola serial interval (SI) from {epiparameter} database
-ebola_si <- epiparameter::epiparameter_db(
+# load a single Ebola onset-to-death delay distribution from {epiparameter}
+# database
+ebola_onset_to_death <- epiparameter::epiparameter_db(
   disease = "Ebola",
-  epi_name = "serial interval",
+  epi_name = "onset-to-death",
   single_epiparameter = TRUE
 )
 
-# check the Ebola SI selected is Gamma, this may change in future versions of
-# {epiparameter} with new entries in the database. The tests will have to be
-# updated if the distribution selected from the database changes.
-family(ebola_si)
+# check the Ebola onset-to-death selected is Gamma, this may change in future
+# versions of {epiparameter} with new entries in the database. The tests will
+# have to be updated if the distribution selected from the database changes
+family(ebola_onset_to_death)
 
-# get parameters from Ebola SI
-dist_params <- epiparameter::get_parameters(ebola_si)
+# get parameters from Ebola onset-to-death
+dist_params <- epiparameter::get_parameters(ebola_onset_to_death)
 
 # load Ebola dataset from {cfr}
 data("ebola1976", package = "cfr")
 
 test_that("cfr_static is works with base and <epiparameter>", {
-  # calculate cfr using Ebola SI parameters with base density
+  # calculate cfr using Ebola onset-to-death parameters with base density
   cfr_base_density <- cfr::cfr_static(
     data = ebola1976,
     delay_density = function(x) {
@@ -25,19 +26,24 @@ test_that("cfr_static is works with base and <epiparameter>", {
     }
   )
 
-  # calculate cfr using Ebola SI parameters with <epiparameter> density
+  # calculate cfr using Ebola onset-to-death parameters with <epiparameter>
+  # density
   cfr_ep_density <- cfr::cfr_static(
     data = ebola1976,
     delay_density = function(x) {
-      density(ebola_si, at = x)
+      density(ebola_onset_to_death, at = x)
     }
   )
 
-  # calculate cfr using Ebola SI parameters with <epiparameter> as.function
-  ebola_si_func <- as.function(ebola_si, func_type = "density")
+  # calculate cfr using Ebola onset-to-death parameters with <epiparameter>
+  # as.function
+  ebola_onset_to_death_func <- as.function(
+    ebola_onset_to_death,
+    func_type = "density"
+  )
   cfr_ep_func_density <- cfr::cfr_static(
     data = ebola1976,
-    delay_density = ebola_si_func
+    delay_density = ebola_onset_to_death_func
   )
 
   # check each density function produces the same cfr
